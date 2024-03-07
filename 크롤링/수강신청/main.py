0e0ada6541adcc858cf3bf2b4fb864ad21fd494c
DainJung <wjdekdls3693@gmail.com>
2024-03-08 01:44:59 +0900

    크롤링 연습

---
+++ 크롤링/수강신청/main.py
@@ -0,0 +1,37 @@
+from playwright.sync_api import sync_playwright
+import time
+from bs4 import BeautifulSoup
+import csv
+
+p = sync_playwright().start()
+
+browser = p.chromium.launch(headless=False)
+
+page = browser.new_page()
+
+# 페이지 열기
+page.goto("https://class.mju.ac.kr/login")
+
+time.sleep(3)
+
+# 팝업 닫기 클릭
+page.click("a.languagehtml")
+
+time.sleep(3)
+
+# 학번 입력
+page.get_by_placeholder("학번을 입력바랍니다.(Student ID)").fill("60192552")
+
+time.sleep(1)
+
+# 비밀번호 입력
+page.get_by_placeholder("SSO 비밀번호를 입력바랍니다. (SSO Password)").fill("wjdekdls8528!")
+
+time.sleep(1)
+
+# 로그인
+page.keyboard.down('Enter')
+
+time.sleep(3)
+
+# 
