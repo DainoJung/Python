8ec6433d41116e8d3ed1ed9a704408693ac2c21a
DainJung <wjdekdls3693@gmail.com>
2024-03-09 19:01:21 +0900

    GPT API 자동 비트코인 매매

--- 크롤링/수강신청/main.py
+++ 크롤링/수강신청/main.py
@@ -1,3 +1,6 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
 from playwright.sync_api import sync_playwright
 import time
 from bs4 import BeautifulSoup
@@ -20,12 +23,12 @@ page.click("a.languagehtml")
 time.sleep(3)
 
 # 학번 입력
-page.get_by_placeholder("학번을 입력바랍니다.(Student ID)").fill("60192552")
+page.get_by_placeholder("학번을 입력바랍니다.(Student ID)").fill(os.getenv("ID"))
 
 time.sleep(1)
 
 # 비밀번호 입력
-page.get_by_placeholder("SSO 비밀번호를 입력바랍니다. (SSO Password)").fill("wjdekdls8528!")
+page.get_by_placeholder("SSO 비밀번호를 입력바랍니다. (SSO Password)").fill(os.getenv("PASSWORD"))
 
 time.sleep(1)
 
