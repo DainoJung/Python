b49273825bde063bd5e11458610844f021b1cc83
DainJung <wjdekdls3693@gmail.com>
2024-03-25 00:35:11 +0900

    지역변수와 전역변수

--- 나도코딩_기본편/practice.py
+++ 나도코딩_기본편/practice.py
@@ -1,6 +1,17 @@
-def profile(name, age, *language):
-    print(f"이름 : {name}\t나이 : {age}", end="\t")
-    for lang in language:
-        print(lang, end=" ")
-profile("유재석", 20, "Python", "Kotlin")
+gun = 10
+
+def checkpoint(soldiers):
+    global gun # 전역 공간에 있는 gun 사용
+    gun = gun - soldiers
+    print(f"[함수 내_전여변수] 남은 총 : {gun}")
     
+    
+def checkpoint2(soldiers):
+    gun = 20 # 지역변수 : 함수 내에서만 사용 가능
+    gun = gun - soldiers
+    print(f"[함수 내_지역변수] 남은 총 : {gun}")
+    
+print(f"전체 총 : {gun}")
+checkpoint(2)
+checkpoint2(2)
+print(f"남은 총 : {gun}")
