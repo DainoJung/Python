d311a99faa0e1d56470ecee8ee819fbcd53a4798
DainJung <wjdekdls3693@gmail.com>
2024-03-25 00:43:12 +0900

    퀴즈 #6

--- 나도코딩_기본편/practice.py
+++ 나도코딩_기본편/practice.py
@@ -1,17 +1,11 @@
-gun = 10
+def std_weight(height, gender):
+    if gender == "men":
+       return height*height*22
+    elif gender == "women":
+        return height*height*21
 
-def checkpoint(soldiers):
-    global gun # 전역 공간에 있는 gun 사용
-    gun = gun - soldiers
-    print(f"[함수 내_전여변수] 남은 총 : {gun}")
-    
-    
-def checkpoint2(soldiers):
-    gun = 20 # 지역변수 : 함수 내에서만 사용 가능
-    gun = gun - soldiers
-    print(f"[함수 내_지역변수] 남은 총 : {gun}")
-    
-print(f"전체 총 : {gun}")
-checkpoint(2)
-checkpoint2(2)
-print(f"남은 총 : {gun}")
+height = 180
+gender = "men"
+
+avg_weight = std_weight(height/100, gender)
+print(avg_weight)
