11a4dbde84334b0213e9e7ae666cfe3cd86dc689
DainJung <wjdekdls3693@gmail.com>
2024-03-25 00:27:12 +0900

    가변인자

--- 나도코딩_기본편/practice.py
+++ 나도코딩_기본편/practice.py
@@ -1,14 +1,6 @@
-from random import *
-
-cnt = 0
-
-for i in range(1, 51):
-    time = randrange(5, 50)
-   
-    if (5 <= time <= 15):
-        cnt += 1
-        print(f"[O] {i}번째 손님 (소요시간 : {time}분")
-    else:
-        print(f"[X] {i}번째 손님 (소요시간 : {time}분")
-
-print(f"총 탑승 승객 : {cnt} 분")        
+def profile(name, age, *language):
+    print(f"이름 : {name}\t나이 : {age}", end="\t")
+    for lang in language:
+        print(lang, end=" ")
+profile("유재석", 20, "Python", "Kotlin")
+    
